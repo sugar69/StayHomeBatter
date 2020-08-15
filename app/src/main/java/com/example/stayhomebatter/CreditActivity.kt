a8dc81a68dc199fa56d77584e3826448049e024c
sugar69 <>
2020-08-15 15:27:51 +0900

    ホーム画面とクレジットの画面遷移を追加

--- app/src/main/java/com/example/stayhomebatter/CreditActivity.kt
+++ app/src/main/java/com/example/stayhomebatter/CreditActivity.kt
@@ -2,10 +2,16 @@ package com.example.stayhomebatter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_credit.*
 
 class CreditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credit)
+
+        //  元の画面に戻る
+        backButton.setOnClickListener{
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/stayhomebatter/MainActivity.kt
+++ app/src/main/java/com/example/stayhomebatter/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.stayhomebatter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //  クレジットの表示
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.credit, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.credit -> {
+                val intent = Intent(this, CreditActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //  ボタンを押すとゲーム画面に遷移
+        gameStartButton.setOnClickListener{
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
